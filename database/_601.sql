3c3cd40b0103ee0afe0368f123025b56cdabe63b
_lpc <wanghaonanlpc@users.noreply.github.com>
2020-09-06 18:27:55 -0400

    601

--- database/_601.sql
+++ database/_601.sql
@@ -1,5 +1,6 @@
-SELECT s1.* FROM stadium AS s1, stadium AS s2, stadium as s3
-    WHERE
+SELECT s1.*
+FROM stadium AS s1, stadium AS s2, stadium as s3
+WHERE
     ((s1.id + 1 = s2.id
     AND s1.id + 2 = s3.id)
     OR
@@ -13,4 +14,4 @@ SELECT s1.* FROM stadium AS s1, stadium AS s2, stadium as s3
     AND s2.people>=100
     AND s3.people>=100
 
-    GROUP BY s1.id
+GROUP BY s1.id
