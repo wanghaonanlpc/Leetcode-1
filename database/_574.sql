12a614b802de018f394c763e6edbc164165faced
_lpc <wanghaonanlpc@users.noreply.github.com>
2020-09-07 16:42:38 -0400

    ss

--- database/_574.sql
+++ database/_574.sql
@@ -1,2 +1,7 @@
-select Name from Candidate as Name where id =
-(select CandidateId from Vote group by CandidateId order by count(CandidateId) desc limit 1);
+select Name
+from Candidate as Name
+where id =
+(select CandidateId
+from Vote
+group by CandidateId
+order by count(CandidateId) desc limit 1);
--- database/_577.sql
+++ database/_577.sql
@@ -1,3 +1,4 @@
-select name, bonus from
-(Employee left join Bonus on Employee.empId = Bonus.empId)
+select name, bonus
+from
+    (Employee left join Bonus on Employee.empId = Bonus.empId)
 where (bonus < 1000 or bonus is null);
